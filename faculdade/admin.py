215e1116af198cddeee290768b42753d5bdf8fe2
vmonteiro2004@gmail.com <vmonteiro2004@gmai.com>
2024-03-06 11:33:21 -0300

    add model, migrations, django admin

--- faculdade/admin.py
+++ faculdade/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
+from faculdade.models import Aluno, Curso
 
-# Register your models here.
+
+class Alunos(admin.ModelAdmin):
+    list_display = ('id', 'nome', 'rg', 'cpf', 'data_nascimento')
+    list_display_links = ('id', 'nome')
+    search_fields = ('id', 'nome', 'rg', 'cpf')
+    list_per_page = 30
+
+admin.site.register(Aluno, Alunos)
+
+
+class Cursos(admin.ModelAdmin):
+    list_display = ('codigo', 'descricao', 'nivel')
+    list_display_links = ('codigo', 'descricao')
+    search_fields = ('codigo',)
+    list_per_page = 30
+
+admin.site.register(Curso, Cursos)
--- faculdade/models.py
+++ faculdade/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
-# Create your models here.
+class Aluno(models.Model):
+    nome = models.CharField(max_length = 50)
+    rg = models.CharField(max_length = 9)
+    cpf = models.CharField(max_length = 11)
+    data_nascimento = models.DateField()
+
+    def __str__(self):
+        return self.nome
+    
+
+class Curso(models.Model):
+    codigo = models.CharField(max_length = 10)
+    descricao = models.CharField(max_length = 256)
+    NIVEL = (
+        ('B', 'Basico'),
+        ('I', 'Intermediario'),
+        ('A', 'Avancado')
+    )
+    nivel = models.CharField(max_length = 1, choices = NIVEL, blank = False, default = 'B')
+
+    def __str__(self):
+        return self.codigo
